a83a62a0f3d666b72205e582dd41cb9685f52d75
Valérie <valerie.pineau-noel.1@ulaval.ca>
2021-09-01 13:22:38 -0400

    Add functions in functions.py and exceptions in exceptions.py
    Functions and exceptions needed to calculate the contrast function are written. Only need to test them if it works well.

--- Code/functions.py
+++ Code/functions.py
@@ -1,36 +1,32 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import tifffile as tiff
 import scipy.ndimage as simg
+import exceptions as exc
 
 def image(imagepath):
-	tifEnd = imagepath[-3] + imagepath[-2] + imagepath[-1]
-	tiffEnd = imagepath[-4] + imagepath[-3] + imagepath[-2] + imagepath[-1]
-	if tifEnd != "tif" and tiffEnd != "tiff":
-		raise Exception("Image must be a .tif of .tiff")
-	else: 
-		image = tiff.imread(str(imagepath))
-		return image
+	exc.isTifOrTiff(imagepath)
+	exc.isString(imagepath)
+	image = tiff.imread(imagepath) 
+	return image
 
 def gaussianFilter(sigma, image):
-	if image is None or sigma is None:
-		raise Exception("Image and sigma must be defined.")
-	elif type(image) is not np.ndarray:
-		raise Exception("Image must be a numpy.ndarray.")
-	elif type(sigma) is not int or type(sigma) is not float:
-		raise Exception("Sigma must be an int or a float.")
-	else:	
-		imgGaussian = simg.gaussian_filter(image, sigma=sigma)
-		return imgGaussian
+	exc.isANumpyArray(image)
+	exc.isDefined(sigma)
+	exc.isDefined(image)
+	exc.isIntOrFloat(sigma)
 
-def stDevAndMeanCalculationsOnePixel(imageDifference, imageUniform, halfSamplingWindow, pixel): 
-	if imageDifference is None or imageUniform is None or samplingWindow is None:
-		raise Exception("All arguments must be defined.")
-	elif type(imageDifference) is not np.ndarray or type(imageUniform) is not np.ndarray:
-		raise Exception("Images must be numpy.nparray.")
-	elif halfSamplingWondow is not int:
-		raise Exception("halfSamplingWondow must be an int.")
-	elif type(pixel) is not list or len(pixel) != 2:
+	imgGaussian = simg.gaussian_filter(image, sigma=sigma)
+	return imgGaussian
+
+def stDevAndMeanContrast(imageDifference, imageUniform, halfSamplingWindow, pixel): 
+	exc.isANumpyArray(imageDifference)
+	exc.isANumpyArray(imageUniform)
+	exc.isDefined(imageDifference)
+	exc.isDefined(imageUniform)
+	exc.isDefined(halfSamplingWindow)
+	exc.isDefined(pixel)
+	exc.isInt(halfSamplingwindow)
+
+	if type(pixel) is not list or len(pixel) != 2:
 		raise Exception("pixel must be a list of two int.")
 	else: 
 		valuesImgDiff = []
@@ -55,12 +51,14 @@ def stDevAndMeanCalculationsOnePixel(imageDifference, imageUniform, halfSampling
 
 					positionInSamplingWindow[0] = positionInSamplingWindow[0] + 1
 
-			std, mean = np.std(valuesImgDiff), np.mean(valuesImgUniform) 
-			return std, mean
+		std, mean = np.std(valuesImgDiff), np.mean(valuesImgUniform) 
+		return std, mean
 
-
-# ----------
 def contrastCalculation(difference, uniform, samplingWindow):
+	exc.isANumpyArray(difference)
+	exc.isANumpyArray(uniform)
+	exc.isInt(samplingWindow)
+
 	n = int(samplingWindow/2)
 	pixelPosition = [0,0]
 	contrastFunction = np.zeros(shape=(imageDifference.shape[0], imageDifference.shape[1]))
@@ -76,7 +74,7 @@ def contrastCalculation(difference, uniform, samplingWindow):
 				positionInSamplingWindow[1] = 0
 	
 			# Calculations of one pixel
-			stDevDifference, meanUniform = stDevAndMeanCalculationsOnePixel(imageDifference=difference, imageUniform=uniform, halfSamplingWindow=n, pixel=pixelPosition)
+			stDevDifference, meanUniform = stDevAndMeanContrast(imageDifference=difference, imageUniform=uniform, halfSamplingWindow=n, pixel=pixelPosition)
 				
 			contrastInSamplingWindow = stDevDifference/meanUniform
 			contrastArray.append(contrastInSamplingWindow)
@@ -89,19 +87,18 @@ def contrastCalculation(difference, uniform, samplingWindow):
 	return contrastFunction
 
 def contrastCalculationSquared(function):
-	if type(function) is not np.ndarray:
-		raise Exception("function must be a np.ndarray.")
-	else:
-		element1 = 0
-		element2 = 0
-		while element1 < function.shape[0] : 
-			while element2 < function.shape[1]:
-				value = function[element1][element2]
-				function[element1][element2] = value**2
-				element2 += 1
-			element1 += 1
+	exc.isANumpyArray(function)
+	
+	element1 = 0
+	element2 = 0
+	while element1 < function.shape[0] : 
+		while element2 < function.shape[1]:
+			value = function[element1][element2]
+			function[element1][element2] = value**2
+			element2 += 1
+		element1 += 1
 
-		return function
+	return function
 
 
 
