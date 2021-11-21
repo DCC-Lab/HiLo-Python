Correcting the eta calculation
Found out that I'm not calculating the eta right. I should consider the amplitude at the cutoff frequency only and not all of the elements in the fft hi and lo images.

Code/functions.py
@@ -351,7 +351,7 @@ def highpassFilter(low):
 	hi = 1 - low
 	return hi
 
-def estimateEta(speckle, uniform, sigma, ffthi, fftlo):
+def estimateEta(speckle, uniform, sigma, ffthi, fftlo, sig):
 	"""
 	returns:
 	the function eta in numpy.ndarray. Calculations taken from the java code for the HiLo Fiji plugin and from personal communication with Olivier Dupont-Therrien at Bliq Photonics
@@ -402,18 +402,50 @@ def estimateEta(speckle, uniform, sigma, ffthi, fftlo):
 	#eta = cmath.sqrt(numerator / denominator) * 1.2
 
 	#Method 3 : eta is obtained experimentally from the HI and the LO images
+	#numerator = 0
+	#denominator = 0
+	#x = 0
+	#y = 0
+	#while x<ffthi.shape[0]:
+	#	while y<ffthi.shape[1]:
+	#		numerator += np.absolute(ffthi[x][y])
+	#		denominator += np.absolute(fftlo[x][y])
+	#		y += 1
+	#	y = 0
+	#	x += 1
+	#eta = numerator/denominator
+
+	#Method 4 : Eta is obtained experimentally from the HI and the LO images at the cutoff frequency
 	numerator = 0
 	denominator = 0
 	x = 0
 	y = 0
+	n = 0
+	d = 0
+	cutoffhi = np.std(ffthi)*0.01
+	cutofflo = np.std(fftlo)*0.01
+
+	cutoff = 0.18*sig
 	while x<ffthi.shape[0]:
 		while y<ffthi.shape[1]:
-			numerator += np.absolute(ffthi[x][y])
-			denominator += np.absolute(fftlo[x][y])
+			if abs(cutoff-ffthi[x][y].real) < cutoffhi:
+				#print(f"I'm adding this to numerator : {ffthi[x][y]}")
+				numerator += cmath.sqrt(ffthi[x][y].real**2 + ffthi[x][y].imag**2)
+				n += 1
+			if abs(cutoff-fftlo[x][y].real) < cutoffhi:
+				#print(f"Add to den : {fftlo[x][y]}")
+				denominator += cmath.sqrt(fftlo[x][y].real**2 + fftlo[x][y].imag**2)
+				d += 1
 			y += 1
 		y = 0
 		x += 1
+	print(f"N : {n}")
+	print(f"D : {d}")
+	print(f"std and ratio hi : {np.std(ffthi)} and {cutoffhi}")
+	print(f"std and ratio lo : {np.std(fftlo)} and {cutofflo}")
 	eta = numerator/denominator
+
+
 
 	# Tried to normalize eta at some point to see what happened. Doesn't work. 
 	#normEta = np.zeros(shape=(uniform.shape[0], uniform.shape[1]), dtype=np.float64)
@@ -462,10 +494,10 @@ def createHiLoImage(uniform, speckle, sigma, sWindow):
 	HI = np.fft.ifft2(np.fft.ifftshift(fftHI))
 
 	# Estimate the function eta for scaling the frequencies of the low image. Generates a complex number. 
-	eta = estimateEta(speckle=speckle, uniform=uniform, sigma=sigma, fftlo=fftLO, ffthi=fftHI)
+	eta = estimateEta(speckle=speckle, uniform=uniform, sigma=sigma, fftlo=fftLO, ffthi=fftHI, sig=sigma)
 
-	print(f"LO : {LO}{type(LO)}{LO.dtype}")
-	print(f"HI : {HI}{type(HI)}{HI.dtype}")
+	#print(f"LO : {LO}{type(LO)}{LO.dtype}")
+	#print(f"HI : {HI}{type(HI)}{HI.dtype}")
 
 	complexHiLo = eta*LO + HI
 
@@ -475,7 +507,8 @@ def createHiLoImage(uniform, speckle, sigma, sWindow):
 	y = 0
 	while x < complexHiLo.shape[0]:
 		while y < complexHiLo.shape[1]:
-			HiLo[x][y] = complexHiLo[x][y].real + complexHiLo[x][y].imag
+			print(complexHiLo[x][y], complexHiLo[x][y].real, complexHiLo[x][y].imag)
+			HiLo[x][y] = cmath.sqrt(complexHiLo[x][y].real**2 + complexHiLo[x][y].imag**2)
 			y += 1
 		y = 0	
 		x += 1